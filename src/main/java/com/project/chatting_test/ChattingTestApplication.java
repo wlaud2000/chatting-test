001b098887d62b88a1dc9b5f621174d04d465a4e
Jimyeong Kim <kjm021221@icloud.com>
2024-09-21 23:22:12 +0900

    first commit

---
+++ src/main/java/com/project/chatting_test/ChattingTestApplication.java
@@ -0,0 +1,13 @@
+package com.project.chatting_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChattingTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/chatting_test/ChattingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.chatting_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
