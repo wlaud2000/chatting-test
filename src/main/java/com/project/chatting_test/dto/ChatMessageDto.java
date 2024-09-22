5d799343ac9a0d0f49fc446e220414f81915d8b0
Jimyeong Kim <kjm021221@icloud.com>
2024-09-22 11:36:25 +0900

    ✨feat: ChatMessageDto 작성

---
+++ src/main/java/com/project/chatting_test/dto/ChatMessageDto.java
@@ -0,0 +1,16 @@
+package com.project.chatting_test.dto;
+
+import lombok.Builder;
+
+@Builder
+public record ChatMessageDto(
+        MessageType messageType,  // 메시지 타입 (입장, 대화)
+        String roomId,            // 채팅방 ID
+        String sender,            // 보낸 사람
+        String message            // 메시지 내용
+) {
+
+    public enum MessageType {
+        ENTER, TALK
+    }
+}
